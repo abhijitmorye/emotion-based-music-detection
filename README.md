079149a497c7b77adb8b6feca7c42e70cefc9efa
abhijitmorye <abhijitmorye>
2022-12-11 08:25:19 -0500

    Project added to the git

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# emotion-based-music-detection
+# Facial Emotion Based Music Detection Web application
+
+## To run this application, make sure you have installed streamlit on your system.
+
+
+```console
+streamlit run capture.py
+```
+
+This will start the streamlit application on the 8051 port.
+
+
+## Project Description
+
+### Technology Stacks Used
+
+	1. Tensorflow
+	2  Streamlit
+	3. OpenCV
+	4. Python
+	5. Matplotlib
+
+
+### CNN Model is designed with 4 Convolution layers, 3 Maxpool layers and 3 Fully connected layers. This CNN model has given training accuracy of 82% and validation accuracy of around 50%. We added droput to our model to improve accuracy further and achived test accuracy of 50%.
+
+
+### OpenCV and Streamlit libraries are used to capture facial emotions of user using system's webcam. Once the facial emotions are captured, user can click on "Recommend Me a song" to retirve the list of Youtube audio/videos of emotion based songs. Users alos have options to provide which type of songs they want to listen, e.g. Bollywood, Western etc.
---
+++ capture.py
@@ -0,0 +1,107 @@
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPool2D
+import webbrowser
+import streamlit as st
+
+
+##
+st.header("Your own's Emotion Based Music Recommender")
+capture = st.checkbox('Capture My Emotion now!')
+FRAME_WINDOW = st.image([])
+captureEmotion = st.button('Capture My Emotion')
+tracks = st.text_input(
+    'Do you want to listen bollywood songs or western songs')
+# casc_path
+casc_path = 'haarcascade_frontalface_default.xml'
+
+# model definition
+model = Sequential([
+    Conv2D(128, (5, 5), input_shape=(48, 48, 1),
+           activation='relu', padding='same'),
+    MaxPool2D((2, 2)),
+    Dropout(0.2),
+    Conv2D(256, (5, 5), activation='relu', padding='same'),
+    MaxPool2D((2, 2)),
+    Dropout(0.2),
+    Conv2D(256, (3, 3), activation='relu', padding='same'),
+    MaxPool2D((2, 2)),
+    Dropout(0.2),
+    Conv2D(256, (3, 3), activation='relu', padding='same'),
+    # MaxPool2D((2,2)),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(256, activation='relu'),
+    Dense(6, activation='softmax')
+])
+model.load_weights('music_recommendation_model.h5')
+emotion_dict = {0: "angry", 1: "fear", 2: "happy",
+                3: "sadness", 4: "surprise", 5: "neutral"}
+
+# OAuth Bearer Tokeni
+
+authToken = 'BQBwAnjmaJRVHNAfxXfByXXRbGFD4FdjMAvzJdSeMp-bxKgYNCmDX2ONFvzACmb6LEAEk-qLoAFqBZM2ohfUokNYrS4ts13MWWTfLOdDbVrCNq6H4gniGPct_3CICYSa_HYwLH39oNBGCQAwM2n87LeUSlj9SQQFG-FnpC_Qnbv3JKMOAjMPwSFsYr7M-lnnbMQ'
+
+
+def capture_face():
+    mood = 'Happy'
+    faceCascade = cv2.CascadeClassifier(casc_path)
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            roi_gray_frame = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(
+                cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            # print(prediction)
+            # print(cropped_img[0])
+            # print(cropped_img[0].shape)
+            # print(type(cropped_img))
+            maxindex = int(np.argmax(prediction))
+            print(maxindex)
+            mood = emotion_dict[maxindex]
+            cv2.putText(frame, emotion_dict[maxindex], (x+40, y-60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            faceImage = np.squeeze(cropped_img[0], axis=2).astype(np.uint8)
+            cv2.imwrite('faces/face.jpg', faceImage)
+
+        # Display the resulting frame
+        # faceImage = cv2.imshow('Video', frame)
+        FRAME_WINDOW.image(frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        if captureEmotion:
+            # print(captureEmotion)
+            predictSongs(mood)
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def predictSongs(userMood):
+    webbrowser.open(
+        f"https://www.youtube.com/results?search_query=top 10+{tracks}+{userMood}+songs")
+
+
+if capture:
+    capture_face()
